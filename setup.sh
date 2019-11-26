e7e5ffd349a7ee5ce7ddd68e96531e4058154507
Jim <jim17227@users.noreply.github.com>
2019-11-26 12:31:42 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Intended to set up a user's home directory and shell environment.
+# After OS installation the following should be done:
+#   a. set up LastPass in browser.
+#   b. log in to GitHub.com
+#   c. clone the "setup" repository.
+#   d. change to the directory where the "setup" repository was cloned.
+#   e. execute ./setup.sh
+
+echo $0
