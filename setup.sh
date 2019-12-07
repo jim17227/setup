2fb96f5782d8b5372e474d805411cc22c9096fc5
Jim <jim17227@users.noreply.github.com>
2019-12-07 12:59:03 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,4 +8,11 @@
 #   d. change to the directory where the "setup" repository was cloned.
 #   e. execute ./setup.sh
 
-echo $0
+setup_dirs="backup bin etc lib share src tftp tftp/netboot var var/log"
+echo $(pwd)/$0
+find "$HOME" -iname '*git/setup/setup.sh*' 2>/dev/null
+
+if running on a mac
+
+# Install HomeBrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
